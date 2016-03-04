b12a16040c014ee30e2d4710702c732dfc436e1e
Antonio F. G. Sevilla <afgs@ucm.es>
2016-03-04 13:46:12 +0100

    Add a raving (creative?) option to the experiment

--- experiment.py
+++ experiment.py
@@ -9,6 +9,9 @@ import networkx as nx
 import nltk
 from nltk.corpus import wordnet as wn
 
+import random
+from functools import reduce
+
 def get_common_concept(a, b):
     try:
         ssa = wn.synsets(a['concept'])
@@ -46,27 +49,45 @@ def generalize (a, b):
     generalize_rec(G, a, b, 0, 0)
     return G
 
+def rave (g, node=0):
+    sss = wn.synsets(g.node[node]['concept'])
+    opts = [ss.closure(lambda s: s.hyponyms(), depth=2) for ss in sss]
+    opts = sss + [s for o in opts for s in o]
+    alt = random.choice(opts)
+    g.node[node]['concept'] = alt.lemma_names()[0]
+    for n in g[node]:
+        rave (g, n)
+
 
 if __name__ == "__main__":
 
     import argparse, sys
 
     arg_parser = argparse.ArgumentParser(description='Do an experiment')
-    arg_parser.add_argument('s1', help='a sentence')
-    arg_parser.add_argument('s2', help='another sentence')
+    arg_parser.add_argument('sent', nargs='+', help='one or more sentences')
     arg_parser.add_argument('-p', '--print', action='store_true', help='display the concept graph for the generalization')
     arg_parser.add_argument('-t','--transform',help="Transformer module to use",default='transform')
     arg_parser.add_argument('-l','--linearize',help="Linearizing module to use",default='simple_nlg')
+    arg_parser.add_argument('-g','--generalize',action="store_true",help="Generalize two sentences")
+    arg_parser.add_argument('-r','--rave',action="store_true",help="Distort the concept graph by raving")
     args = arg_parser.parse_args()
 
     sys.path.insert(1, 'modules')
 
     T = __import__(args.transform)
-    gs = [transform_tree(parse(s), T.rules) for s in [args.s1, args.s2]]
-    gen = generalize(*gs)
+    gs = [transform_tree(parse(s), T.rules) for s in args.sent]
+
+    if args.generalize:
+        gs = [reduce(generalize, gs)]
+
+    if args.rave:
+        for g in gs:
+            rave(g)
 
     if args.print:
-        draw_concept_graph(gen)
+        for g in gs:
+            draw_concept_graph(g)
 
     L = __import__(args.linearize)
-    print(L.linearize(gen))
+    for g in gs:
+        print(L.linearize(g))
