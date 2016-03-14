638ccd9b9b2fa226230dc97659f9f61ac4973a13
Antonio F. G. Sevilla <afgs@ucm.es>
2016-03-14 14:33:17 +0100

    Accept stdin to summarize too

--- summary.py
+++ summary.py
@@ -40,15 +40,20 @@ if __name__ == "__main__":
     import argparse, sys
 
     arg_parser = argparse.ArgumentParser(description='Summarize text via concept graphs')
-    arg_parser.add_argument('text', help='Text file to summarize')
+    arg_parser.add_argument('text', nargs='*', help='Text file to summarize')
     arg_parser.add_argument('-t','--transform',help="Transformer module to use",default='transform')
     arg_parser.add_argument('-l','--linearize',help="Linearizing module to use",default='simple_nlg')
     args = arg_parser.parse_args()
 
     sys.path.insert(1, 'modules')
 
+    if len(args.text)>0:
+        text = ' '.join(args.text)
+    else:
+        text = sys.stdin.read()
+
     T = __import__(args.transform)
-    cg = CG(grammar=T.grammar, text=args.text)
+    cg = CG(grammar=T.grammar, text=text)
     extend(cg)
     for cl in cop.cluster(cg).clusters:
         cg.draw(cl)
