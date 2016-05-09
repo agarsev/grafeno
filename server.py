0427d0686dc7ed11458469b24bd86253bcc2d40b
Antonio F. G. Sevilla <afgs@ucm.es>
2016-05-09 12:50:32 +0200

    Trying to filter input

--- server.py
+++ server.py
@@ -2,9 +2,11 @@
 
 import argparse
 from bottle import abort, error, post, request, run
+import re
+import unicodedata
 
 from conceptgraphs import Graph as CG
-from conceptgraphs.transformers import get_pipeline
+from conceptgraphs.transformers import get_pipeline as get_transformers
 
 arg_parser = argparse.ArgumentParser(description='REST server for concept graphs')
 arg_parser.add_argument('-H','--hostname',help='hostname to bind to',default='localhost')
@@ -12,17 +14,24 @@ arg_parser.add_argument('-P','--port',type=int,help='port number to bind to',def
 arg_parser.add_argument('-t','--default-transformer',help='transformer to use by default',default='deep')
 args = arg_parser.parse_args()
 
+#all_chars = (unichr(i) for i in xrange(0x110000))
+#control_chars = ''.join(c for c in all_chars if unicodedata.category(c) == 'Cc')
+control_chars = ''.join([chr(x) for x in range(0,32)] + [chr(x) for x in range(127,160)])
+control_char_re = re.compile('[%s]' % re.escape(control_chars))
+def remove_control_chars(s):
+    return control_char_re.sub(' ', s)
+
 @post('/')
 def extract():
     req = request.json
-    text = req.get('text')
+    text = remove_control_chars(req.get('text'))
     trans = req.get('transformers', args.default_transformer)
     try:
         trans = [t.strip() for t in trans.split(',')]
     except AttributeError:
         pass
     try:
-        T = get_pipeline(trans)
+        T = get_transformers(trans)
     except KeyError:
         abort(400,"Invalid transformers")
     graph = CG(transformer=T,transformer_args=req.get('args',{}),text=text)
@@ -32,7 +41,7 @@ def extract():
 def custom400 (error):
     return json.dumps({
         'error': True,
-        'message': error.body
+        'error_message': error.body
         })
 
 run(host=args.hostname,port=args.port)
