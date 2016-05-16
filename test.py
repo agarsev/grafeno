b5efde0866f7bc52ce2303221a51ad4af82cb697
Antonio F. G. Sevilla <afgs@ucm.es>
2016-05-17 00:57:59 +0200

    Better ux for tester

--- test.py
+++ test.py
@@ -3,17 +3,17 @@
 import argparse
 from bottle import abort, error, post, request, run
 
-from conceptgraphs import Graph as CG
-import conceptgraphs.transformers as transformers
-import conceptgraphs.linearizers as linearizers
+from conceptgraphs import Graph as CG, transformers, linearizers
+
+arrayize = lambda t: t.split(',')
 
 arg_parser = argparse.ArgumentParser(description='Test script')
 group = arg_parser.add_mutually_exclusive_group()
 group.add_argument('-s','--string')
 group.add_argument('-f','--file',type=argparse.FileType('r'))
-arg_parser.add_argument('-t','--transformers',action='append',help='transformer pipeline to use')
+arg_parser.add_argument('-t','--transformers',type=arrayize,help='transformer pipeline to use',default=['semantic'])
+arg_parser.add_argument('-l','--linearizers',type=arrayize,help='linearizing pipeline to use',default=[])
 arg_parser.add_argument('-p','--print-json',action='store_true',help='print json instead of displaying')
-arg_parser.add_argument('-l','--linearizers',action='append',help='linearizing pipeline to use',default=[])
 args = arg_parser.parse_args()
 
 if args.file:
