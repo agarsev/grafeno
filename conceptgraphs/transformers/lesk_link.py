d725b0f4678d97102693aed0f074eb81a7d1d83c
Antonio F. G. Sevilla <afgs@ucm.es>
2016-05-20 16:46:29 +0200

    Finished adaptation

--- conceptgraphs/transformers/lesk_link.py
+++ conceptgraphs/transformers/lesk_link.py
@@ -1,3 +1,4 @@
+from itertools import product
 import pexpect
 
 from .sentences import Transformer as SentRecord
@@ -7,29 +8,30 @@ class Transformer (WNGet, SentRecord):
 
     def __init__ (self, sim_threshold = 100, sim_weight = 1, **kwds):
         super().__init__(**kwds)
-        self.sim_threshold = sim_threshold
-        self.sim_weight = sim_weight
-        self.child = pexpect.spawn("/usr/bin/env WNSEARCHDIR=/usr/share/wordnet similarity.pl --type WordNet::Similarity::lesk --interact")
-        self.child.delaybeforesend = 0
+        self.__threshold = sim_threshold
+        self.__weight = sim_weight
+        self.__child = pexpect.spawn("/usr/bin/env WNSEARCHDIR=/usr/share/wordnet similarity.pl --type WordNet::Similarity::lesk --interact")
+        self.__child.delaybeforesend = 0
 
     def post_insertion (self, sentence_nodes):
         super().post_insertion(sentence_nodes)
         g = self.graph
-        threshold = self.sim_threshold
-        weight = self.sim_weight
-        child = self.child
+        threshold = self.__threshold
+        weight = self.__weight
+        child = self.__child
         oldnodes = set(n for s in g.gram['sentences'] for n in s)
-        for n in sentence_nodes:
-            for m in oldnodes:
-                sa = g._g.node[n]['concept']
-                sb = g._g.node[m]['concept']
-                child.expect("Concept #1:")
-                child.sendline(sa)
-                child.expect("Concept #2:")
-                child.sendline(sb)
-                match = child.expect([' ([0-9.]+)\r\n', 'not found'])
-                if match == 0:
-                    sim = int(child.match.group(1))
-                    if sim > threshold:
-                        g.add_edge(n, m, 'SIM', weight=weight)
-                        g.add_edge(m, n, 'SIM', weight=weight)
+        for n, m in product(sentence_nodes, oldnodes):
+            if m == n:
+                continue
+            sa = g.node[n]['concept']
+            sb = g.node[m]['concept']
+            child.expect("Concept #1:")
+            child.sendline(sa)
+            child.expect("Concept #2:")
+            child.sendline(sb)
+            match = child.expect([' ([0-9.]+)\r\n', 'not found'])
+            if match == 0:
+                sim = int(child.match.group(1))
+                if sim > threshold:
+                    g.add_edge(n, m, 'SIM', weight=weight)
+                    g.add_edge(m, n, 'SIM', weight=weight)
--- conceptgraphs/transformers/sim_link.py
+++ conceptgraphs/transformers/sim_link.py
@@ -1,34 +1,35 @@
 from .sentences import Transformer as SentRecord
 from .wordnet import Transformer as WNGet
 
+from itertools import product
+
 from nltk.corpus import wordnet_ic
 brown_ic = wordnet_ic.ic('ic-brown.dat')
 
 class Transformer (WNGet, SentRecord):
 
-    def __init__ (self, sim_threshold = 0.01, sim_weight = 1, **kwds):
+    def __init__ (self, sim_threshold = 0.1, sim_weight = 1, **kwds):
         super().__init__(**kwds)
-        self.sim_threshold = sim_threshold
-        self.sim_weight = sim_weight
+        self.__threshold = sim_threshold
+        self.__weight = sim_weight
 
     def post_insertion (self, sentence_nodes):
         super().post_insertion(sentence_nodes)
         g = self.graph
-        threshold = self.sim_threshold
-        weight = self.sim_weight
-        oldnodes = set(n for s in self.gram['sentences'] for n in s)
-        for n in sentence_nodes:
-            for m in oldnodes:
-                if m == n:
-                    continue
-                ga = g._g.node[n]
-                gb = g._g.node[m]
-                if 'synset' not in ga or 'synset' not in gb \
-                        or ga.get('sempos','x') != gb.get('sempos','-'):
-                    continue
-                sa = ga.get('synset')
-                sb = ga.get('synset')
-                sim = sa.jcn_similarity(sb, brown_ic)
-                if sim > threshold:
-                    g.add_edge(n, m, 'SIM', weight=weight)
-                    g.add_edge(m, n, 'SIM', weight=weight)
+        threshold = self.__threshold
+        weight = self.__weight
+        oldnodes = set(n for s in g.gram['sentences'] for n in s)
+        for n, m in product(sentence_nodes, oldnodes):
+            if m == n:
+                continue
+            ga = g.node[n]
+            gb = g.node[m]
+            if 'synset' not in ga or 'synset' not in gb \
+                    or ga.get('sempos','x') != gb.get('sempos','-'):
+                continue
+            sa = ga.get('synset')
+            sb = gb.get('synset')
+            sim = sa.jcn_similarity(sb, brown_ic)
+            if sim > threshold:
+                g.add_edge(n, m, 'SIM', weight=weight)
+                g.add_edge(m, n, 'SIM', weight=weight)
