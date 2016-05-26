15575484dde89ad9c5b5a80e164abd7749d1d8ce
Antonio F. G. Sevilla <afgs@ucm.es>
2016-05-26 18:36:09 +0200

    Some small aesthetic improvements.

--- conceptgraphs/transformers/keep_deps.py
+++ conceptgraphs/transformers/keep_deps.py
@@ -8,15 +8,16 @@ default_deps = {
 
 class Transformer (Base):
 
-    def __init__ (self, dep_translate = default_deps, **kwds):
+    def __init__ (self, dep_translate = default_deps, unknown_dep_translate = '', **kwds):
         super().__init__(**kwds)
         self.__deps = dep_translate.keys()
         self.__translate = dep_translate
+        self.__unknown = unknown_dep_translate
 
     def transform_dep (self, dep, pid, cid):
         edge = super().transform_dep(dep, pid, cid)
         p = self.nodes[pid]
         c = self.nodes[cid]
         if 'concept' in p and 'concept' in c:
-            edge['functor'] = self.__translate.get(dep, 'NONE')
+            edge['functor'] = self.__translate.get(dep, self.__unknown)
         return edge
