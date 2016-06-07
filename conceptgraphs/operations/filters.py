15164bdbd2dbbc36f597db1ea1d24df0ec2303fd
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2016-06-07 17:32:54 +0200

    Filter edges by frequency

--- conceptgraphs/operations/filters.py
+++ conceptgraphs/operations/filters.py
@@ -1,12 +1,18 @@
-def filter_edges (cgraph, remove=[], rename={}):
+from collections import defaultdict
+
+def filter_edges (cgraph, remove=[], rename={}, frequency=None):
     g = cgraph._g
     to_rem = []
-    for n in g:
-        for m in g[n]:
-            ftor = g[n][m]['functor']
-            if ftor in rename:
-                g[n][m]['functor'] = rename[ftor]
-            if ftor in remove:
-                to_rem.append((n,m))
+    if frequency:
+        freqs = defaultdict(lambda:0)
+        for n, m, d in g.edges_iter(data=True):
+            freqs[d['functor']] += 1
+    for n, m, d in g.edges_iter(data=True):
+        ftor = d['functor']
+        if frequency and (freqs[ftor]>frequency['max'] or freqs[ftor]<frequency['min']):
+            to_rem.append((n,m))
+        if ftor in rename:
+            d['functor'] = rename[ftor]
+        if ftor in remove:
+            to_rem.append((n,m))
     g.remove_edges_from(to_rem)
-
