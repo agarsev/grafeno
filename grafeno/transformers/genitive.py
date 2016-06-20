16a3c3076377f7320cfd53e79725b6df739b7d3f
Antonio F. G. Sevilla <afgs@ucm.es>
2016-06-20 13:41:06 +0200

    Optional genitive attach

--- grafeno/transformers/genitive.py
+++ grafeno/transformers/genitive.py
@@ -1,12 +1,15 @@
 from grafeno.transformers.__utils import Transformer as Utils
 
 default_add_genitive_class = True
+default_attach_genitive = True
 
 class Transformer (Utils):
 
-    def __init__ (self, **kwds):
+    def __init__ (self, add_genitive_class = default_add_genitive_class,
+            attach_genitive = default_attach_genitive, **kwds):
         super().__init__(**kwds)
-        self.__addclass = kwds.get('add_genitive_class', default_add_genitive_class)
+        self.__addclass = add_genitive_class
+        self.__attach = attach_genitive
 
     def transform_node (self, ms):
         sem = super().transform_node(ms)
@@ -32,5 +35,6 @@ class Transformer (Utils):
         if 'genitive_obj' in c and p.get('sempos') != 'v' and 'concept' in p:
             if self.__addclass:
                 self.sprout(parent, 'isa', {'concept':p['concept'], 'sempos':p.get('sempos')})
-            p['concept'] += '_of_' + c['genitive_obj']
+            if self.__attach:
+                p['concept'] += '_of_' + c['genitive_obj']
         return edge
