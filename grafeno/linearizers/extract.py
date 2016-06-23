07f99270f91e66924c9c00d20ee370aafb145396
Antonio F. G. Sevilla <afgs@ucm.es>
2016-06-23 21:06:58 +0200

    Normalize sentence scores based on length for extractive summaries

--- grafeno/linearizers/extract.py
+++ grafeno/linearizers/extract.py
@@ -1,16 +1,21 @@
 class Linearizer ():
 
-    def __init__ (self, summary_length=100, summary_margin=10, graph=None):
+    def __init__ (self, summary_length=100, summary_margin=10,
+            normalize_sentence_scores=False, graph=None):
         self.graph = graph
         self.__length = summary_length
         self.__margin = summary_margin
+        self.__norm = normalize_sentence_scores
 
     def score_sentence (self, sentence_nodes):
         return 0
 
     def linearize (self):
         g = self.graph
-        sentence_scores = [self.score_sentence(s) for s in g.gram['sentence_nodes']]
+        if self.__norm:
+            sentence_scores = [self.score_sentence(s)/len(s) if len(s) else 0 for s in g.gram['sentence_nodes']]
+        else:
+            sentence_scores = [self.score_sentence(s) for s in g.gram['sentence_nodes']]
 
         best = sorted(range(len(sentence_scores)), reverse=True, key=lambda i:sentence_scores[i])
         length = 0
