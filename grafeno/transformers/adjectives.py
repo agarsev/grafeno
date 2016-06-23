e578709386eaf17c2a12c5038fff5777e854ad52
Antonio F. G. Sevilla <afgs@ucm.es>
2016-06-23 17:08:30 +0200

    Documenting & refactoring: adjectives

---
+++ grafeno/transformers/adjectives.py
@@ -0,0 +1,41 @@
+from grafeno.transformers.pos_extract import Transformer as PosExtract
+from grafeno.transformers.__utils import Transformer as Utils
+
+class Transformer (PosExtract, Utils):
+    '''Processes adjectives. Adds an ``'ATTR'`` functor relation to the head noun.
+
+    Parameters
+    ----------
+    attach_adjectives : bool
+        Attaches the adjectival concept to the head noun concept. Useful to
+        distinguish nominal nodes when specified by modifiers.
+    attached_adjective_hyper : bool
+        If both `attach_adjectives` and `attached_adjective_hyper` are true, an
+        hypernym node is added to the head with the original nominal concept.
+    keep_attached_adj : bool
+        If `attach_adjectives` is True and `keep_attached_adj` is False,
+        adjectival nodes are dropped after being attached.
+    '''
+
+    def __init__ (self, attach_adjectives = False, attached_adjective_hyper = True, keep_attached_adj = False, **kwds):
+        super().__init__(**kwds)
+        self.__attach = attach_adjectives
+        self.__hyper = attached_adjective_hyper
+        self.__keep = keep_attached_adj
+
+    def transform_dep (self, dep, parent, child):
+        edge = super().transform_dep(dep, parent, child)
+        p = self.nodes[parent]
+        c = self.nodes[child]
+        if dep == 'ncmod' and 'concept' in p and 'concept' in c and p.get('sempos') == 'n' and c.get('sempos') == 'j':
+            if self.__attach:
+                if self.__hyper:
+                    self.sprout(parent, 'HYP', {'concept':p['concept'], 'sempos':'n'})
+                p['concept'] = c['concept']+'_'+p['concept']
+                if self.__keep:
+                    edge['functor'] = 'ATTR'
+                else:
+                    del c['concept']
+            else:
+                edge['functor'] = 'ATTR'
+        return edge
--- grafeno/transformers/all.py
+++ grafeno/transformers/all.py
@@ -1,14 +1,20 @@
 from grafeno.transformers.base import Transformer as Base
 
 class Transformer (Base):
+    '''This transformer carries over all morphological nodes and syntactic
+    dependencies to the semantic level. It is good for developing/debugging
+    purposes, since it directly translates the dependency tree into the semantic
+    graph.'''
 
     def transform_node (self, msnode):
+        '''The concept is the lemma of the morphological node.'''
         sem = super().transform_node(msnode)
         sem.update(msnode)
         sem['concept'] = msnode['lemma']
         return sem
 
     def transform_dep (self, dependency, parent, child):
+        '''The functor is the dependency name.'''
         edge = super().transform_dep(dependency, parent, child)
         edge['functor'] = dependency
         return edge
