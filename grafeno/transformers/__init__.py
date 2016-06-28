72661cba7c5dd04a020df74a5c3657e261dfa2f9
Antonio F. G. Sevilla <afgs@ucm.es>
2016-06-28 15:14:14 +0200

    Documenting the transformers

--- grafeno/transformers/__init__.py
+++ grafeno/transformers/__init__.py
@@ -14,6 +14,78 @@
 # You should have received a copy of the GNU Affero General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+'''
+:py:mod:`Transformers <grafeno.transformers.base>` are one of the key objects of
+the `grafeno` library. They are in charge of converting the dependency parse of
+a sentence, extracted by an external tool, into a `grafeno` semantic graph.
+
+::
+
+    from grafeno import Graph as CG
+    from grafeno.transformers import get_pipeline
+
+    T = get_pipeline(['pos_extract', 'wordnet', 'unique'])
+    g = CG(transformer=T, transformer_args={}, text="Fish fish fish fish fish fish fish.")
+
+This process happens in stages. First, morphological nodes are transformed
+into semantic ones:
+
+.. _semantic_nodes:
+
+.. rubric:: Semantic nodes
+
+Semantic nodes are dictionaries with the following
+attributes:
+
+- ``concept``: if present, the node will be added to the semantic
+  graph. It represents the main idea, or meaning, of the node. If there is no
+  `concept`, no semantic node will be produced corresponding to the
+  morphological one.
+- ``id``: a temporal identifier for the node while it is being
+  processed, and hasn't thus been added to the graph yet. When the node
+  is finally added to the graph, it will be changed to the proper graph
+  ID.
+
+Other attributes in the dictionary are also added to the
+semantic graph node, and are referred to as `grammatemes`.
+
+After the nodes have been processed, the dependency relations are transformed
+into semantic edges:
+
+.. _semantic_edges:
+
+.. rubric:: Semantic edges
+
+Each semantic edge is a dictionary with the following attributes:
+
+- ``parent``: the (temporal or otherwise) id of the source node.
+- ``child``: the (temporal or otherwise) id of the target node.
+- ``functor``: if present, the edge will be added to the semantic graph. The
+  `functor` represents the semantic relation between the `parent` and `child`
+  nodes.
+
+Other attributes in the dictionary will be also added to the
+semantic graph edge, and are referred to as `grammatemes`.
+
+Apart from the main operations of node and edge transformations, there are
+additional stages in the process where previous or further processing can
+happen. In order to construct this collection of processing stages, a
+`Transformer` object has to be created. For this, a :py:mod:`base <grafeno.transformers.base>`
+class is provided, which has methods for the different stages and is in charge
+of calling them at the right time and with the appropriate arguments.
+
+The way to construct a pipeline is thus to inherit from this :py:mod:`base <grafeno.transformers.base>`
+class, and extend the appropriate methods. See its documentation for more
+information on them.
+
+Additionally, the idea behind transformer classes is that each is supposed to
+perform a specific operation. This way, a transforming pipeline can be
+constructed by mixing and matching the desired transformers, by way of creating
+a class which inherits from them. In order to make this operation easier, a
+convenience function is provided: :py:mod:`grafeno.transformers.get_pipeline`,
+which takes a list of transformers to use, and constructs the appropriate class
+which inherits from them all in the correct order.
+'''
 
 import glob
 from importlib import import_module
@@ -27,7 +99,7 @@ for m in glob.glob(dirname(__file__)+"/*.py"):
         transformers[f] = import_module('grafeno.transformers.'+f, __name__).Transformer
 
 def get_pipeline (modules):
-    '''Takes a list of transformers and returns a transformer which
+    '''Takes a list of transformers and returns a transformer class which
     subclasses them all'''
     name = '__'.join(modules)
     if name in transformers:
--- grafeno/transformers/base.py
+++ grafeno/transformers/base.py
@@ -1,55 +1,114 @@
 from collections import deque
 
 class Transformer:
+    '''This class is the basic transformer class. Other transformers should
+    inherit from it either directly or indirectly.
+
+    Transformer composition in `grafeno` uses cooperative inheritance. When a
+    new module is written, it should extend the base class, or it can extend one
+    or more other transformers which provide some required functionality. This
+    is a way of managing dependencies, since the base classes will be inserted
+    by Python into the inheritance chain.
+
+    The new module can then extend the methods it is interested in, adding
+    some processing to that stage. However, for the chaining to work, every
+    extended method has to make sure to:
+
+    1. Call ``super()`` with the correct (original) arguments at the very beggining of the function body.
+    2. Return the appropriate value, either modified, or untouched as returned from ``super()``.
+
+    Some attributes are present in the transformer during processing. They can
+    be used and modified in the appropriate stages.
+
+    Parameters
+    ----------
+    graph : :py:mod:`Graph <grafeno.graph>`
+        The graph to which all transformed text will be added.
+
+    Attributes
+    ----------
+    graph : :py:mod:`Graph <grafeno.graph>`
+        The graph to which all transformed text will be added.
+    stage : string
+        Useful for debugging or checking, this string denotes what stage the
+        processing is currently in.
+    nodes : dict of :ref:`semantic nodes <semantic_nodes>`
+        Available from pre_process up to post_process.
+        Map of semantic nodes obtained for the sentence being processed, indexed
+        by id.
+    edges : list of :ref:`semantic edges <semantic_edges>`
+        Available from pre_process up to post_process.
+        List of semantic edges obtained for the sentence being processed.
+    '''
 
     def __init__ (self, graph=None, **kwds):
         self.stage = ""
         self.graph = graph
 
     def transform_text (self, sentences):
+        '''Transforms a list of sentences into the semantic graph.
+
+        It shouldn't be overriden.'''
         self.stage = "before_all"
         self.before_all()
         for s in sentences:
-            self.transform_sentence(s)
+            self._transform_sentence(s)
         self.stage = "after_all"
         self.after_all()
         self.stage = ""
 
-    def transform_sentence (self, tree):
-        '''Transform the tree according to the rules and add
-        resulting nodes and edges to the graph'''
+    def _transform_sentence (self, tree):
         self.stage = "pre_process"
         self.pre_process(tree)
         self.stage = "process_nodes"
-        self.process_nodes(tree)
+        self._process_nodes(tree)
         self.stage = "extract_dependencies"
-        deps = self.extract_dependencies(tree)
+        deps = self._extract_dependencies(tree)
         deps.reverse()
         self.stage = "process_edges"
-        self.process_edges(deps)
+        self._process_edges(deps)
         self.stage = "post_process"
         self.post_process()
         self.stage = "add_to_graph"
-        sentence_nodes = self.add_to_graph()
+        sentence_nodes = self._add_to_graph()
         self.stage = "post_insertion"
         self.post_insertion(sentence_nodes)
         self.stage = ""
 
     def pre_process (self, tree):
+        '''Prepares the transformer for processing a new sentence. Transformers
+        can extend this method to initialize per-sentence variables.
+
+        Parameters
+        ----------
+        tree : dict
+            The dependency parse of the sentence.
+        '''
         self.nodes = {} # ID -> dict { concept }
         self.edges = [] # dict { parent(ID), child(ID), functor }
 
-    def process_nodes (self, tree):
+    def _process_nodes (self, tree):
         for ms in tree['tokens']:
             self.nodes[ms['id']] = self.transform_node(ms)
 
     def transform_node (self, msnode):
-        '''take a morfosyntactic node (dict) and return a semantic node (dict).
-        Semantic nodes get dropped in the end if they don't have a concept
-        attribute.'''
+        '''Transform a morphosyntactic node to a semantic one.
+
+        Transformers should extend this module if any processing should occur
+        for individual nodes.
+
+        Parameters
+        ----------
+        msnode : dict
+            Dictionary of morphosyntactic tags
+
+        Returns
+        -------
+        :ref:`semantic node <semantic_nodes>`
+        '''
         return { 'id': msnode['id'] }
 
-    def extract_dependencies (self, tree):
+    def _extract_dependencies (self, tree):
         root = tree['dependencies'][0]
         deps = deque([(c, root['token']) for c in root.get('children', [])])
         ret = []
@@ -63,7 +122,7 @@ class Transformer:
                 deps.append((c,d['token']))
         return ret
 
-    def process_edges (self, deps):
+    def _process_edges (self, deps):
         for name, parent, child in deps:
             try:
                 self.edges.append(self.transform_dep(name, parent, child))
@@ -71,17 +130,37 @@ class Transformer:
                 continue
 
     def transform_dep (self, dependency, parent, child):
-        '''take a dependency relation (str) and the parent and child node ids
-        and return a semantic edge (dict). Semantic edges get
-        dropped in the end if they don't have a functor attribute.'''
+        '''Transforms a dependency relation into a semantic edge.
+
+        Transformers should extend this module if any processing should occur
+        for each dependency relation.
+
+        Parameters
+        ----------
+        dependency : string
+            Name of the dependency relation.
+        parent, child : int
+            Temporary ids of the source and target semantic nodes. Note that
+            these provisional nodes might not turn into true semantic nodes in
+            the graph, if they don't have a `concept` attribute by the end of
+            processing.
+
+        Returns
+        -------
+        :ref:`semantic edge <semantic_edges>`
+        '''
         return { 'parent': parent,
                  'child': child }
 
     def merge (self, a, b):
-        '''combine two nodes by id. Update all outgoing and incoming edges. All
+        '''TODO: maybe this should be moved to the utils transformer.
+
+        Combine two nodes by id. Update all outgoing and incoming edges. All
         properties of b are lost, the ones in a are kept. a can be an existing
         graph node, b should be a node currently being processed.
-        Can be done only during post_process.'''
+
+        .. note:: Can be done only during post_process.
+        '''
         if self.stage != 'post_process':
             raise AssertionError("merge must be called during post_process")
         del self.nodes[b]
@@ -94,9 +173,18 @@ class Transformer:
                 del edge['functor']
 
     def post_process (self):
+        '''This method is called after all nodes and dependency relations are
+        processed. Sentence-level processing should be done here, as well as any
+        node or edge merging or destruction.
+
+        Even though there are no parameters or return values, extenders should
+        still call ``super()`` at the beggining. Semantic nodes and edges are
+        available in the transformer's (self) ``nodes`` and ``edges``
+        properties.
+        '''
         pass
 
-    def add_to_graph (self):
+    def _add_to_graph (self):
         g = self.graph
         real_id = {}
         for id, node in self.nodes.items():
@@ -112,10 +200,22 @@ class Transformer:
         return list(real_id.values())
 
     def post_insertion (self, sentence_nodes):
+        '''Called after the processed nodes and edges are added to the semantic
+        graph. It is useful if some processing needs the real graph ids of the
+        new nodes.
+
+        Parameters
+        ----------
+        sentence_nodes : list of ids
+            The definitive (graph) ids of the nodes that were produced by
+            analyzing the current sentence.
+        '''
         pass
 
     def before_all (self):
+        '''Called at the beggining of processing a full text, before any sentences.'''
         pass
 
     def after_all (self):
+        '''Called at the end of processing a full text, after all sentences.'''
         pass
